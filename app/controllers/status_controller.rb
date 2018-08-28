391a4119addf17f0db273c7ea37fc4f1eca0a9ec
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-28 16:30:01 -0600

    email confirmation seems to be working

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,9 +1,13 @@
 class StatusController < ApplicationController
   def index
     @user = User.find_by(user_token: params[:token])
-    @user.status = true
-    binding.pry
+    @user.update({status: true})
     @user.save!
     redirect_to '/dashboard'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password_digest, :user_token, :email, :status).merge(status: true)
   end
 end
