ad93b0eb759e6c17c1ebbb028cfa7898c763d21a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-28 18:53:56 -0600

    trying to get the spec harness to run

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::GamesController < ActionController::API
-
   def show
     game = Game.find(params[:id])
     if game != nil
       render json: game
     else
-      render status: 400
+      render status:400
     end
+  end
+
+  def create
   end
 end
