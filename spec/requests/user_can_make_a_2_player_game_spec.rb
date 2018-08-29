0722e77cf5dcc459210dcabb13ed8dca3cb3a6b3
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-29 16:07:07 -0600

    starting to test player can create game

---
+++ spec/requests/user_can_make_a_2_player_game_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Battleshift' do
+  context '2 player game' do
+    it 'can make a two player game' do
+      user_1 = User.create(username: 'Seth', email: 'sschwartz214@gmail.com', password_digest: 's', user_token: 'AyYClax4fvejaSjrdtfQ3Q', status: true)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+
+      get '/dashboard'
+      expect(response).to be_successful
+      expect(page).to have_content('Status: Active')
+
+      
+    end
+  end
+end
