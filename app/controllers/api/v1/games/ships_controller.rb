b7c09831f1451d3fd41203e0ad068a6924942966
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-08-30 14:47:46 -0600

    Ship placement on spec harness working

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::Games::ShipsController < ApiController
   def create
+    @user = set_user
     ship = Ship.new(params[:ship][:ship_size])
     ship.place(params[:ship][:start_space], params[:ship][:end_space])
     game = Game.find_by(id: params["game_id"])
+    current_user = game.users.find_by(user_token: @user.user_token)
     ShipPlacer.new(board: game.player_1_board, ship: ship, start_space: ship.start_space, end_space: ship.end_space).run
-    render json: game, message: "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
+    render json: game, message: set_message(params[:ship][:ship_size])
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,17 @@
 class ApiController < ActionController::API
   def set_user
     api_key = request.headers["HTTP_X_API_KEY"]
-    @user = User.find_by(user_token: )
+    user = User.find_by(user_token: api_key)
+    user
+  end
+
+  def set_message(ship_length)
+    if ship_length == 3
+      "Successfully placed ship with a size of 3. You have 1 ship(s) to place with a size of 2."
+    elsif ship_length == 2
+      "Successfully placed ship with a size of 2. You have 0 ship(s) to place."
+    else
+      "YOU IDOT"
+    end
   end
 end
