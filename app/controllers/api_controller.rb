88c7d8e10889b3ff96dab6b526b3307fcb2ae6d2
Seth Schwartz <sschwartz214@gmail.com>
2018-08-30 12:06:51 -0600

    comments out WIP code

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ActionController::API
   def set_user
     api_key = request.headers["HTTP_X_API_KEY"]
-    @user = User.find_by(user_token: )
+    # @user = User.find_by(user_token: )
   end
 end
