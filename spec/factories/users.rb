1a6b8cb817fbb076b36d904dbe8e429213e44b19
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-03 16:09:52 -0600

    fixes login test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
-  factory :user do
-    username { 'eddie_brock' }
-    email { 'something@something.com' }
-    password_digest { 'ducktales' }
-    user_token { '007' }
-    status { true }
-    identifier { '007' }
+ factory :user do
+   username { 'eddie_brock' }
+   email { 'spiderman2isbetterthanspidermanhomecoming@doctoroctopus.com' }
+   password { 'ducktales' }
+   user_token { '007' }
+   status { true }
 
     factory :user_2 do
       username { 'wade_wilson' }
       email { 'doctor@octagonapus.com' }
-      password_digest { 'darkwing_duck' }
+      password { 'darkwing_duck' }
       user_token { '009' }
       status { true }
-      identifier { '009' }
     end
   end
 end
