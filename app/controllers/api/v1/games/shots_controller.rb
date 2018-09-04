eed3c65b05c266bc49cad07ff320ad0e30e2452a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-09-03 22:57:21 -0600

    controllers refactored

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -1,27 +1,19 @@
 class Api::V1::Games::ShotsController < ApiController
-  include ShipChecker
   before_action :check_headers, :current_turn_check, :winner?
 
   def create
     game = Game.find(params[:game_id])
-    @user = set_user
-    if set_player == "player_1"
-      game.current_turn = set_player
-      turn_processor = TurnProcessor.new(game, params[:shot][:target])
+    shots = ShotsPresenter.new(game, params[:shot][:target], set_player)
+    ship_status = shots.complete_turn(set_user.email)
+    render_turn_outcome(ship_status, game)
+  end
+
+  private
+  def render_turn_outcome(status, game)
+    if status != "Invalid coordinates."
+      render json: game, message: status
     else
-      game.current_turn = set_player
-      turn_processor = TurnProcessor.new(game, params[:shot][:target])
-    end
-    if turn_processor.run! == true
-      if set_player == "player_1"
-        @addon = check_ship_status(game, game.player_2_board.board, @user.email)
-      else
-        @addon = check_ship_status(game, game.player_1_board.board, @user.email)
-      end
-      @whole = turn_processor.message + @addon
-      render json: game, message: @whole
-    else
-      render json: game, status:400, message: "Invalid coordinates"
+      render json: game, status:400, message: status
     end
   end
 end
